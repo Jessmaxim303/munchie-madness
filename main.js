8d08303d02ccd049efd030823b98ee991435c38b
Jesse Maxim <jessmaxim303@gmail.com>
2019-09-01 15:13:26 -0600

    Final function completed

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ var snackDesc = document.getElementById('snack-desc');
 var snackTitle = document.querySelector('.snack__title');
 
 addSnackButton.addEventListener('click', snackCard);
+groceryList.addEventListener('click', deleteCard);
+snackTitle.addEventListener('keyup', buttonActiveState);
 
 function snackCard(e){
-	console.log('hi');
 	e.preventDefault();
-	// groceryList.innerText = "hi";
+console.log('Snack Card');
  groceryList.insertAdjacentHTML('afterbegin',
  `<section class="munchie__card">
  <h1 id="snack__title--h1"> 
@@ -17,8 +18,28 @@ function snackCard(e){
  <textarea id="snack__textarea">
  ${snackDesc.value}
  </textarea>
- <button id="delete__button"></button>
+ <img class="delete__button" src="images/delete.svg">
  </section>`)
+ 	clearForms()
+}
+
+function deleteCard(e){
+	if(e.target.classList.contains("delete__button")){
+		e.target.parentNode.remove();
+	}
+}
+
+function buttonActiveState(e){
+	console.log('function connention')
+	addSnackButton.disabled = false;
+}
+
+function clearForms(){
+	snackTitle.value = '';
+	snackDesc.value = '';
+	snackTitle.placeholder = 'Enter snack item here...';
+	snackDesc.placeholder = 'Add snack info here!';
+	addSnackButton.disabled = true;
 }
 
 //Iteration 1
@@ -27,10 +48,8 @@ function snackCard(e){
 //Iteration 2
 // Edit the HTML of the dynamic snack cards to include a DELETE button. When the user clicks the DELETE button, that specific CARD is removed from the DOM
 
-
 //Iteration 3
 //User can edit the name of the snack OR its description by clicking on the particular field on the CARD!
-
 
 //Iteration 4
 // If there is no text in the item field OR the info field, the Add Snack button should be disabled
